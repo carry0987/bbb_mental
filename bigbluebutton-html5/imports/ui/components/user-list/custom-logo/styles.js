d81626e33f1daa5f18e82ce01b73e69a61be6365
carry0987 <carry0987@users.noreply.github.com>
2023-10-27 14:08:00 +0800

    Update styles.js

--- bigbluebutton-html5/imports/ui/components/user-list/custom-logo/styles.js
+++ bigbluebutton-html5/imports/ui/components/user-list/custom-logo/styles.js
@@ -14,7 +14,7 @@ const Branding = styled.div`
   padding: ${smPaddingX};
   width: 100%;
   & > img {
-    max-height: 3rem;
+    max-height: 6rem;
     max-width: 100%;
     display: block;
     margin-left: auto;

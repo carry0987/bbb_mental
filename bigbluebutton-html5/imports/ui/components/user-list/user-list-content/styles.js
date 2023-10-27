ad5b92c7ac86c4115504782c40b4c6603181444e
carry0987 <carry0987@users.noreply.github.com>
2023-10-27 19:34:17 +0800

    Update styles.js

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/styles.js
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/styles.js
@@ -112,7 +112,7 @@ const ListItem = styled(Styled.ListItem)`
     flex-grow: 1;
     line-height: 2;
     text-align: left;
-    padding-left: ${lgPaddingY};
+    /* padding-left: ${lgPaddingY}; */
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
